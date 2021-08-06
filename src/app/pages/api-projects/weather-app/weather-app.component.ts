d953791491487fc2863a153b920f8f9ac6630f83
Rayven Deray <rjderay@gmail.com>
2021-08-05 19:46:59 -0500

    RW-2 added title, temperature, and weather condition image

--- src/app/pages/api-projects/weather-app/weather-app.component.ts
+++ src/app/pages/api-projects/weather-app/weather-app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiControllerService } from 'src/app/common/api-controller.service';
+import { Weather } from 'src/assets/data/api-classes/weather';
 
 @Component({
   selector: 'app-weather-app',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-app.component.scss']
 })
 export class WeatherAppComponent implements OnInit {
+  public weather: Weather = { temp: -999, city: "", type: "", icon: "http://openweathermap.org/img/wn/" };
+  public loadedWeather: boolean = false;
+  public errorWeather: boolean = false;
 
-  constructor() { }
+  constructor(
+    private _apiService: ApiControllerService
+  ) { }
 
   ngOnInit(): void {
+    this.getLocalWeather();
   }
+
+  public async getLocalWeather(): Promise<void>{
+    navigator.geolocation.getCurrentPosition( async (pos) => {
+      try{
+        const fetched = await this._apiService.getTemp(pos.coords.latitude, pos.coords.longitude)
+        this.weather.icon += fetched.weather[0].icon + ".png";
+        this.weather.city = fetched.name;
+        this.weather.type = fetched.weather[0].description.split(" ").map( (word: any) => word.charAt(0).toUpperCase() + word.substring(1)).join(" ");
+        this.weather.temp = Math.round(fetched.main.temp);
+        this.loadedWeather = true;
+      }catch(error){
+        this.errorWeather = true;
+        this.loadedWeather = true;
+      }
+    },
+      () =>{
+        this.errorWeather = true;
+        this.loadedWeather = true;
+      }
+    );
+  }
+
 
 }
