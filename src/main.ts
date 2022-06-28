4d15e0b2ee3f96a873ece441a74c7f1dce977803
Rayven Deray <rjderay@gmail.com>
2022-06-27 19:29:38 -0500

    RW-6 bootstrappign!

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { APP_INITIALIZER, enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +8,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+bootstrap();
